a442637afe8d65f6212cd8e17f49dd474f2876d0
unknown <LAB_SI_B_404_PC3@B404-03.upc.edu.pe>
2015-04-10 21:23:52 -0500

    Conectar a BD

---
+++ Sem3-Ses1/Sem3-Ses1/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem3_Ses1
+{
+    class Conexion
+    {
+        public static string URL = "Data Source=.;Initial Catalog=NORTHWND;Integrated Security=True";
+
+    }
+}
---
+++ Sem3-Ses1/Sem3-Ses1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace Sem3_Ses1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Permite conectarme a una BD
+            SqlConnection sqlCon = new SqlConnection();
+            //A qué BD voy a conectar
+            sqlCon.ConnectionString = Conexion.URL;
+            //Abrir la conexion para ejecutar comandos SQL
+            sqlCon.Open();
+            Console.WriteLine("Se conectó a la BD " + sqlCon.ConnectionString);
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = sqlCon;
+            command.CommandText = "INSERT INTO Categories(CategoryName) VALUES ('PRUEBA')";
+            command.CommandType = CommandType.Text;
+            command.ExecuteNonQuery();
+            //Cerrar la conexion
+            sqlCon.Close();
+            Console.WriteLine("Se inserto correctamente la categoria");
+            Console.ReadLine();
+        }
+    }
+}
